c16cdd11323e993e2a2cd363efa29ec12942ae92
ishitasavera1706 <22030501046@darshan.ac.in>
2023-12-31 11:25:21 +0530

    Feat : Button Added for Product in Admin

--- Admin/product.php
+++ Admin/product.php
@@ -1,3 +1,5 @@
 <?php include_once('partials/main.php') ?>
-<div>product</div>
+
+<a class="pl-3" href="add_product.php"><button type="button" class="btn btn-social-icon-text btn-facebook"><i class="mdi mdi-plus"></i>Add Product</button></a>
+
 <?php include_once('partials/footer.php') ?>    
