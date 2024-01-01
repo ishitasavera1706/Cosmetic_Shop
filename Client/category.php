afd8e91efe57319b2ae53bfd76fb179107a87038
ishitasavera1706 <22030501046@darshan.ac.in>
2024-01-01 19:23:28 +0530

    category in client

---
+++ Client/category.php
@@ -0,0 +1,66 @@
+<?php include_once('partials/header.php') ?>
+<?php include_once('partials/navbar.php') ?>
+<section class="section mt-5" id="products">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-heading">
+                        <h2>Our Latest Products</h2>
+                        <span>Check out all of our products.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="item">
+                        <div class="thumb">
+                            <div class="hover-content">
+                                <ul>
+                                    <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+                                </ul>
+                            </div>
+                            <img src="assets/images/men-01.jpg" alt="">
+                        </div>
+                        <div class="down-content">
+                            <h4>Classic Spring</h4>
+                            <span>$120.00</span>
+                            <ul class="stars">
+                                <li><i class="fa fa-star"></i></li>
+                                <li><i class="fa fa-star"></i></li>
+                                <li><i class="fa fa-star"></i></li>
+                                <li><i class="fa fa-star"></i></li>
+                                <li><i class="fa fa-star"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="item">
+                        <div class="thumb">
+                            <div class="hover-content">
+                                <ul>
+                                    <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+                                </ul>
+                            </div>
+                            <img src="assets/images/men-01.jpg" alt="">
+                        </div>
+                        <div class="down-content">
+                            <h4>Classic Spring</h4>
+                            <span>$120.00</span>
+                            <ul class="stars">
+                                <li><i class="fa fa-star"></i></li>
+                                <li><i class="fa fa-star"></i></li>
+                                <li><i class="fa fa-star"></i></li>
+                                <li><i class="fa fa-star"></i></li>
+                                <li><i class="fa fa-star"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+<?php include_once('partials/footer.php') ?>	
